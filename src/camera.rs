8037bbc35ab51bb446aa3a53058a32fc1e8394db
Nick Bryan <nickbryan.dev@gmail.com>
2020-09-17 16:51:18 +0100

    Add basic zoom/Y movement

--- src/camera.rs
+++ src/camera.rs
@@ -1,3 +1,4 @@
+use bevy::input::mouse;
 use bevy::prelude::*;
 
 pub struct CameraPlugin;
@@ -6,7 +7,9 @@ impl Plugin for CameraPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app.add_startup_system(setup.system())
             .init_resource::<MoveSystemState>()
-            .add_system(move_system.system());
+            .add_system(move_system.system())
+            .init_resource::<ZoomSystemState>()
+            .add_system(zoom_system.system());
     }
 }
 
@@ -18,7 +21,7 @@ const STARTING_PITCH: f32 = -15.0;
 fn setup(mut commands: Commands) {
     commands
         .spawn(Camera3dComponents {
-            translation: Translation::new(25.0, 10.0, 25.0),
+            translation: Translation::new(25.0, 20.0, 25.0),
             rotation: Rotation::from_rotation_yxz(
                 STARTING_YAW.to_radians(),
                 STARTING_PITCH.to_radians(),
@@ -74,8 +77,50 @@ fn move_system(
     }
 
     for (_, mut translation, rotation) in &mut camera_query.iter() {
+        // Account for camera direction.
         movement = rotation.mul_vec3(movement);
+
         *translation.x_mut() += movement.x() * time.delta_seconds;
         *translation.z_mut() += movement.z() * time.delta_seconds;
+    }
+}
+
+const ZOOM_SPEED: f32 = 0.5;
+const MAX_ZOOM: f32 = 30.0;
+const MIN_ZOOM: f32 = 10.0;
+
+#[derive(Default)]
+struct ZoomSystemState {
+    mouse_wheel_event_reader: EventReader<mouse::MouseWheel>,
+}
+
+fn zoom_system(
+    mut state: ResMut<ZoomSystemState>,
+    events: Res<Events<mouse::MouseWheel>>,
+    time: Res<Time>,
+    mut camera_query: Query<(&CameraComponent, &mut Translation)>,
+) {
+    for event in state.mouse_wheel_event_reader.iter(&events) {
+        if let mouse::MouseScrollUnit::Pixel = event.unit {
+            for (_, mut translation) in &mut camera_query.iter() {
+                let current = translation.y();
+                let movement = (event.y * ZOOM_SPEED) * time.delta_seconds;
+                let new_value = current + movement;
+
+                if new_value < MIN_ZOOM {
+                    translation.set_y(MIN_ZOOM);
+                    return;
+                }
+
+                if new_value > MAX_ZOOM {
+                    translation.set_y(MAX_ZOOM);
+                    return;
+                }
+
+                translation.set_y(new_value);
+            }
+        } else {
+            panic!("we currently only deal with pixel units on mouse scroll");
+        }
     }
 }
